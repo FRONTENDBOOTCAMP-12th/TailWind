f768b81ebc14edd06b8eb4eeac01ccb6a7feccf4
workerKwon <dmdmdm1322@gmail.com>
2024-12-13 11:58:16 +0900

    Feature : checkbox 컴포넌트 개발
    - 체크박스 컴포넌트와 이벤트 개발
    - 체크박스 스타일 적용
    
    resolves #42

--- src/components/modal/modalStyle.js
+++ src/components/modal/modalStyle.js
@@ -109,44 +109,6 @@ export const modalStyles = css`
                 .checkbox-group {
                     position: relative;
                     padding-left: 90px;
-
-                    input[type='checkbox'] {
-                        margin: 0;
-                        position: absolute;
-                        width: 24px;
-                        height: 24px;
-                        appearance: none;
-                    }
-
-                    label {
-                        display: inline-block;
-                        line-height: 24px;
-                        padding-left: 30px; // SVG 공간 확보
-                    }
-
-                    svg {
-                        position: absolute;
-                        width: 24px;
-                        height: 24px;
-
-                        circle {
-                            fill: #fff;
-                            stroke: #ddd;
-                        }
-
-                        path {
-                            fill: #ddd;
-                        }
-                    }
-
-                    input[type='checkbox']:checked ~ svg circle {
-                        fill: var(--primary);
-                        stroke: none;
-                    }
-
-                    input[type='checkbox']:checked ~ svg path {
-                        fill: #fff;
-                    }
                 }
             }
 
