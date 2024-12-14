617d5c96420ae43fff7fd5b19c96cfdb9c228195
Littlestar0508 <rist9860@gmail.com>
2024-12-14 22:02:40 +0900

    Feat: cart페이지 장바구니 컴포넌트 디자인 구현
    - html css만 렌더링 -> 접근성 고려X
    - 로직 작성 x
    - 동적 렌더링 x

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -11,6 +11,7 @@ class Cart extends LitElement {
         productFrozen: { type: Array },
         productChilled: { type: Array },
         productTemperature: { type: Array },
+        user: { type: Object },
     };
 
     constructor() {
@@ -35,6 +36,10 @@ class Cart extends LitElement {
         this.productFrozen = this.productList.filter((index) => index.productTreat === 'frozen');
         this.productChilled = this.productList.filter((index) => index.productTreat === 'chilled');
         this.productTemperature = this.productList.filter((index) => index.productTreat === 'temperature');
+
+        this.user = {
+            address: '서울 중랑구 면목로 42길 11 (행운빌딩) 603호',
+        };
     }
 
     render() {
@@ -56,11 +61,16 @@ class Cart extends LitElement {
                             </div>
                             <img src="/assets/dropdown-arrow.svg" />
                         </li>
-                        ${this.productFrozen.map(
+                        <!-- TODO : 라디오 컴포넌트 삽입 -->
+                        ${this.productChilled.map(
                             (idx) =>
-                                html`<img src="${idx.img}" /> <span>${idx.productName}</span> <span>${idx.price.toLocaleString()}원</span>
+                                html` <div class="cart-product">
+                                    <img class="cart-product-image" src="${idx.img}" />
+                                    <span class="cart-product-title">${idx.productName}</span>
                                     <inc-dec-btn></inc-dec-btn>
-                                    <img src="/assets/product-cancel.svg" /> `
+                                    <span class="cart-product-price">${idx.price.toLocaleString()}원</span>
+                                    <img class="cart-product-delete" src="/assets/product-cancel.svg" />
+                                </div>`
                         )}
                         <li>
                             <div class="food-category-container">
@@ -69,6 +79,16 @@ class Cart extends LitElement {
                             </div>
                             <img src="/assets/dropdown-arrow.svg" />
                         </li>
+                        ${this.productFrozen.map(
+                            (idx) =>
+                                html` <div class="cart-product">
+                                    <img class="cart-product-image" src="${idx.img}" />
+                                    <span class="cart-product-title">${idx.productName}</span>
+                                    <inc-dec-btn></inc-dec-btn>
+                                    <span class="cart-product-price">${idx.price.toLocaleString()}원</span>
+                                    <img class="cart-product-delete" src="/assets/product-cancel.svg" />
+                                </div>`
+                        )}
                         <li>
                             <div class="food-category-container">
                                 <img src="/assets/temperature.svg" />
@@ -76,6 +96,16 @@ class Cart extends LitElement {
                             </div>
                             <img src="/assets/dropdown-arrow.svg" />
                         </li>
+                        ${this.productFrozen.map(
+                            (idx) =>
+                                html` <div class="cart-product">
+                                    <img class="cart-product-image" src="${idx.img}" />
+                                    <span class="cart-product-title">${idx.productName}</span>
+                                    <inc-dec-btn></inc-dec-btn>
+                                    <span class="cart-product-price">${idx.price.toLocaleString()}원</span>
+                                    <img class="cart-product-delete" src="/assets/product-cancel.svg" />
+                                </div>`
+                        )}
                         <li>
                             <div class="product-check-container">
                                 <img src="/assets/product-check.svg" />
@@ -83,7 +113,51 @@ class Cart extends LitElement {
                             </div>
                         </li>
                     </ul>
-                    <div class="purchase-container"></div>
+                    <div class="purchase-container">
+                        <section class="purchase-address">
+                            <h1 class="address-title">
+                                <img src="/assets/place-pin.svg" />
+                                <span>배송지</span>
+                            </h1>
+                            <p class="address-info">${this.user.address}</p>
+                            <p class="delivery-text">샛별배송</p>
+                            <button class="delivery-btn">배송지 변경</button>
+                        </section>
+                        <div class="purchase-price">
+                            <div class="purchase-price-detail">
+                                <div>
+                                    <span>상품금액</span>
+                                    <span>40,680 <b>원</b></span>
+                                </div>
+                                <div>
+                                    <span>상품할인금액</span>
+                                    <span>-4,651 <b>원</b></span>
+                                </div>
+                                <div>
+                                    <span>배송비</span>
+                                    <span>+3,000 <b>원</b></span>
+                                </div>
+                            </div>
+                            <div class="purchase-info">
+                                <div class="purchase-total-price">
+                                    <span>결제예정금액</span>
+                                    <span><b>40,680</b>원</span>
+                                </div>
+                                <div class="purchase-saving">
+                                    <span>적립</span>
+                                    <span>최대 36원 적립 일반 0.1%</span>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" class="purchase-confirm">주문하기</button>
+                        <div class="purchase-detail">
+                            쿠폰/적립금은 주문서에서 사용 가능합니다<br />
+                            [주문완료] 상태일 경우에만 주문 취소 가능합니다.<br />
+                            [마이컬리 > 주문내역 상세페이지] 에서 직접 취소하실 수 있습니다.<br />
+                            쿠폰, 적립금 사용 금액을 제외한 실 결제 금액 기준으로 최종 산정됩니다.<br />
+                            상품별로 적립금 지급 기준이 다를 수 있습니다. (상품 상세 페이지에서 확인 가능합니다)<br />
+                        </div>
+                    </div>
                 </div>
             </section>
         `;
--- src/components/cart/cartStyle.js
+++ src/components/cart/cartStyle.js
@@ -47,12 +47,152 @@ export default css`
                     }
                 }
             }
+        }
+    }
 
-            .purchase-container {
-                background-color: yellow;
-                width: 100px;
-                height: 100px;
+    .purchase-container {
+        width: 17.75rem;
+
+        .purchase-address {
+            padding: 1.25rem;
+            border: 1px solid var(--gray--100);
+            display: flex;
+            flex-direction: column;
+            gap: 0.75rem;
+            font-weight: 600;
+
+            .address-title {
+                color: var(--black);
+                font-size: var(--label---medium);
+                display: flex;
+                align-items: center;
             }
+
+            .address-info {
+                color: var(--black);
+                font-size: var(--label---medium);
+            }
+
+            .delivery-text {
+                color: var(--primary);
+                font-size: var(--label---small);
+            }
+
+            .delivery-btn {
+                width: 15.25rem;
+                height: 2.75rem;
+                border: 1px solid var(--primary);
+                border-radius: 0.25rem;
+                text-align: center;
+                color: var(--primary);
+                font-size: var(--label---medium);
+            }
+        }
+
+        .purchase-price {
+            padding: 1.25rem;
+            border: 1px solid var(--gray--100);
+            background-color: var(--gray--50);
+
+            .purchase-price-detail {
+                display: flex;
+                flex-direction: column;
+                gap: 1rem;
+                padding-bottom: 1.5rem;
+                border-bottom: 2px dotted var(--gray--100);
+                font-size: var(--paragraph---medium);
+
+                div span:nth-child(2) {
+                    float: right;
+
+                    b {
+                        font-size: var(--label---medium);
+                        font-weight: 600;
+                    }
+                }
+            }
+
+            .purchase-info {
+                padding-top: 1.5rem;
+                display: flex;
+                flex-direction: column;
+                color: var(--black);
+
+                .purchase-total-price {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+
+                    span:nth-child(2) {
+                        font-weight: 600;
+                        b {
+                            font-size: var(--heading---x-l);
+                        }
+                    }
+                }
+
+                .purchase-saving {
+                    display: flex;
+                    gap: 0.5rem;
+                    justify-content: right;
+                    font-weight: 600;
+                    margin-top: 0.625rem;
+
+                    span:nth-child(1) {
+                        background-color: var(--accent--yellow);
+                        color: var(--white);
+                        border-radius: 1px;
+                        padding-inline: 0.5rem;
+                        font-size: var(--label---small);
+                    }
+
+                    span:nth-child(2) {
+                        font-size: var(--label---small);
+                    }
+                }
+            }
+        }
+
+        .purchase-confirm {
+            width: 100%;
+            height: 2.75rem;
+            background-color: var(--primary);
+            color: var(--white);
+            text-align: center;
+            border-radius: 0.25rem;
+            margin-top: 1rem;
+        }
+
+        .purchase-detail {
+            margin-top: 1rem;
+            color: var(--gray--400);
+            font-size: var(--label---small);
+            font-weight: 600;
+        }
+    }
+    .cart-product {
+        display: flex;
+        gap: 0.5rem;
+        height: 7.375rem;
+        align-items: center;
+
+        .cart-product-image {
+            width: 3.75rem;
+            height: 4.875rem;
+        }
+
+        .cart-product-title {
+            width: 21.5625rem;
+        }
+
+        .cart-product-price {
+            width: 7.9375rem;
+            text-align: right;
+        }
+
+        .cart-product-delete {
+            width: 1.875rem;
+            height: 1.875rem;
         }
     }
 `;
