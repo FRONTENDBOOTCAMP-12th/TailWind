69fde7936a6ebd5635d3a845d96d50c0a852710e
workerKwon <dmdmdm1322@gmail.com>
2024-12-16 16:37:49 +0900

    Feature : 포켓호스트 적용
    - 포켓베이스 설치
    - 포켓호스트 api url 세팅
    - 포켓호스트 예시 적용
    
    resolves #71

--- src/pages/product-detail/index.js
+++ src/pages/product-detail/index.js
@@ -2,3 +2,25 @@ import '@/layout/header.js';
 import '@/components/modal/modal.js';
 import '@/components/label/label.js';
 import '@/components/radio-group/radioGroup.js';
+import { LitElement, html } from 'lit';
+import PocketBase from 'pocketbase';
+
+class ProductDetail extends LitElement {
+    constructor() {
+        super();
+        this.pb = new PocketBase(import.meta.env.VITE_API_URL);
+    }
+
+    async connectedCallback() {
+        super.connectedCallback();
+        const product = await this.pb.collection('product_detail_info').getOne('l8125u60nj73e27');
+        console.log(product);
+        this.product = product;
+    }
+
+    render() {
+        return html`<div>상품 상세</div>`;
+    }
+}
+
+customElements.define('product-detail', ProductDetail);
