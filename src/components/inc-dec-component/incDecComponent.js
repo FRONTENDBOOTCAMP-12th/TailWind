5d23796e39c1525a9d557fa04f26dc6efbc39b1b
Littlestar0508 <rist9860@gmail.com>
2024-12-17 09:54:27 +0900

    Modify: 증감 컴포넌트의 로직 수정
    - 하나의 id에 하나의 증감 컴포넌트 값만 처리하도록 수정
    - 장바구니 페이지와 아닌 곳을 분리하여 처리 방식 수정
    - localStorage를 이용하여 값을 공유하도록 수정

--- src/components/inc-dec-component/incDecComponent.js
+++ src/components/inc-dec-component/incDecComponent.js
@@ -8,43 +8,37 @@ class IncDecBtn extends LitElement {
     static properties = {
         id: { type: String, attribute: 'id' },
         idValue: { type: Number },
-        items: { type: Number },
         cartItems: { type: Object },
-        isCartPage: { type: Boolean, attribute: 'cartpage', reflect: true },
+        inCartPage: { type: Boolean, attribute: 'incartpage', reflect: true },
     };
+
+    constructor() {
+        super();
+        this.inCartPage = false;
+    }
 
     connectedCallback() {
         super.connectedCallback();
-        console.log(this.isCartPage);
+        this.inCartPage = this.getAttribute('incartpage') === 'true' ? true : false;
         this.cartItems = JSON.parse(localStorage.getItem('cartItems')) ?? {};
-        this.isCartPage ? (this.idValue = this.cartItems[`${this.id}`]) : (this.idValue = 0);
-        this.items = this.idValue ?? 0;
+        this.inCartPage ? (this.idValue = this.cartItems[`${this.id}`] ?? 0) : (this.idValue = 0);
     }
 
-    // 매개변수로 add를 받았다면 localStorage에 itemQuantity를 1증가 혹은 감소 시키는 함수
-    // 현재 increase와 decrease에 겹치는 부분을 외부 함수로 따로 분리
-    // TOPIC : 현재 매개변수로는 add만을 인식하고 그에 따라 동작하는데 이를 if else문 혹은 swich문으로 작성하는게 좀 더 가독성이 좋을까?
     calcQuantity(calc) {
         this.cartItems = JSON.parse(localStorage.getItem('cartItems')) ?? {};
-        console.log(this.isCartPage);
         if (calc === 'add') {
-            console.log(this.cartItems);
-            this.items += 1;
-            this.cartItems[`${this.id}`] = this.items ?? 0;
+            this.idValue += 1;
         } else {
-            this.items -= 1;
-            this.cartItems[`${this.id}`] = this.items ?? 0;
+            this.idValue -= 1;
         }
-        this.isCartPage ? localStorage.setItem('cartItems', JSON.stringify(this.cartItems)) : localStorage.setItem(`${this.id}`, this.items);
+        this.cartItems[`${this.id}`] = this.idValue;
+        this.inCartPage ? localStorage.setItem('cartItems', JSON.stringify(this.cartItems)) : localStorage.setItem(`${this.id}`, this.idValue);
     }
 
-    // 1증가 시키고 rerender시키는 함수
-    // TOPIC : rerender시키는게 성능적으로 괜찮을까?
     handleIncreaseItem() {
         this.calcQuantity('add');
     }
 
-    // 1감소 시키고 rerender시키는 함수
     handleDecreaseItem() {
         this.calcQuantity('minus');
     }
@@ -52,12 +46,11 @@ class IncDecBtn extends LitElement {
     render() {
         return html`
             <div class="amount-container">
-                <!--만약 제품 수량이 1 미만이라면 - 버튼 비활성화-->
                 <button
                     type="button"
                     class="minus-button"
                     @click=${this.handleDecreaseItem}
-                    ?disabled=${this.items < 1}
+                    ?disabled=${this.idValue < 1}
                     aria-label="제품 수량 감소 버튼"
                 >
                     <svg width="10" height="2" viewBox="0 0 10 2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -65,7 +58,7 @@ class IncDecBtn extends LitElement {
                     </svg>
                 </button>
                 <span class="sr-only">제품 수량</span>
-                <span class="item-amount">${this.items}</span>
+                <span class="item-amount">${this.idValue}</span>
                 <button type="button" class="plus-button" @click=${this.handleIncreaseItem} aria-label="제품 수량 증가 버튼">
                     <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6 0V4H10V6H6V10H4V6H0V4H4V0H6Z" fill="currentColor" />
