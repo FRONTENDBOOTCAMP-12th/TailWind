e38144723a4de25c961be8d69aee90fca10ae6be
Littlestar0508 <rist9860@gmail.com>
2024-12-17 13:41:07 +0900

    Modify: api호출 테스트를 위한 push
    - 이후 conflict 충돌 방지를 위해 되돌릴 예정

--- src/components/inc-dec-component/incDecComponent.js
+++ src/components/inc-dec-component/incDecComponent.js
@@ -5,50 +5,73 @@ import incDecComponentStyle from '@/components/inc-dec-component/incDecComponent
 class IncDecBtn extends LitElement {
     static styles = [reset, incDecComponentStyle];
 
-    // 기본적으로 localStorage에 값을 항상 0으로 시작
+    static properties = {
+        id: { type: String, attribute: 'id' },
+        idValue: { type: Number },
+        cartItems: { type: Object },
+        inCartPage: { type: Boolean, attribute: 'incartpage', reflect: true },
+        // id:해당 inc-dec-btn 컴포넌트에 할당될 id
+        // idValue : 그 id에 대응하는 값. 만약 null or undefined라면 0
+        // cartItems : 현재 localStorage에 저장된 카트 정보
+        // inCartPage : 이 컴포넌트가 장바구니 페이지에서 사용되는지에 대한 여부
+    };
+
     constructor() {
         super();
-        localStorage.setItem('itemQuantity', 0);
+        this.inCartPage = false;
     }
 
-    // 매개변수로 add를 받았다면 localStorage에 itemQuantity를 1증가 혹은 감소 시키는 함수
-    // 현재 increase와 decrease에 겹치는 부분을 외부 함수로 따로 분리
-    // TOPIC : 현재 매개변수로는 add만을 인식하고 그에 따라 동작하는데 이를 if else문 혹은 swich문으로 작성하는게 좀 더 가독성이 좋을까?
+    connectedCallback() {
+        super.connectedCallback();
+        // 만약 값을 true로 전달받았다면 true를 그 외에는 false로 처리
+        this.inCartPage = this.getAttribute('incartpage') === 'true' ? true : false;
+        // localStorage에 cartItems가 없다면 빈 객체 할다
+        this.cartItems = JSON.parse(localStorage.getItem('cartItems')) ?? {};
+        // 만약 장바구니 페이지라면 저장된 value를, 아니라면 0을 할당
+        this.inCartPage ? (this.idValue = this.cartItems[`${this.id}`] ?? 0) : (this.idValue = 0);
+    }
+
     calcQuantity(calc) {
-        const quantity = localStorage.getItem('itemQuantity');
-        calc === 'add' ? localStorage.setItem('itemQuantity', Number(quantity) + 1) : localStorage.setItem('itemQuantity', Number(quantity) - 1);
+        // 새로운 localStorage의 값을 참조하기 위한 할당
+        this.cartItems = JSON.parse(localStorage.getItem('cartItems')) ?? {};
+        if (calc === 'add') {
+            this.idValue += 1;
+        } else {
+            this.idValue -= 1;
+        }
+
+        // localStorage에 값을 변경(저장X)
+        this.cartItems[`${this.id}`] = this.idValue;
+        // 장바구니 페이지라면 객체를 저장, 아니라면 id를 key로 저장
+        this.inCartPage ? localStorage.setItem('cartItems', JSON.stringify(this.cartItems)) : localStorage.setItem(`${this.id}`, this.idValue);
     }
 
-    // 1증가 시키고 rerender시키는 함수
-    // TOPIC : rerender시키는게 성능적으로 괜찮을까?
     handleIncreaseItem() {
         this.calcQuantity('add');
-        this.requestUpdate();
     }
 
-    // 1감소 시키고 rerender시키는 함수
     handleDecreaseItem() {
         this.calcQuantity('minus');
-        this.requestUpdate();
     }
 
     render() {
         return html`
             <div class="amount-container">
-                <!--만약 제품 수량이 1 미만이라면 - 버튼 비활성화-->
+                <!-- 해당 수량이 0이라면 마이너스 버튼은 비활성화 -->
                 <button
                     type="button"
                     class="minus-button"
                     @click=${this.handleDecreaseItem}
-                    ?disabled=${localStorage.getItem('itemQuantity') < 1}
+                    ?disabled=${this.idValue < 1}
                     aria-label="제품 수량 감소 버튼"
                 >
                     <svg width="10" height="2" viewBox="0 0 10 2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10 0V2H0V0H10Z" fill="currentColor" />
                     </svg>
                 </button>
+                <!--접근성 향상을 위한 텍스트-->
                 <span class="sr-only">제품 수량</span>
-                <span class="item-amount">${localStorage.getItem('itemQuantity')}</span>
+                <span class="item-amount">${this.idValue}</span>
                 <button type="button" class="plus-button" @click=${this.handleIncreaseItem} aria-label="제품 수량 증가 버튼">
                     <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6 0V4H10V6H6V10H4V6H0V4H4V0H6Z" fill="currentColor" />
@@ -60,3 +83,6 @@ class IncDecBtn extends LitElement {
 }
 
 customElements.define('inc-dec-btn', IncDecBtn);
+// 사용 예시
+// 카트 페이지 내부 : <inc-dec-btn id=`${불러오는 제품의 id값}` incartpage="true"></inc-dec-btn>
+// 카트 페이지 제외 : <inc-dec-btn id=`${불러오는 제품의 id값}` incartpage="false"></inc-dec-btn>
