7f3e88e9e0f057a87e1f0942b8d1c1bfca3e0048
lyb9030 <lyb_9030@naver.com>
2024-12-19 09:34:58 +0900

    feat : package.json 코드 추가

--- src/components/swiper/swiper.js
+++ src/components/swiper/swiper.js
@@ -9,7 +9,7 @@ class ProductSwiper extends LitElement {
     render() {
         return html`
             <swiper-container autoplay delay="100" disableOnInteraction="false" navigation="true" pagination="true" loop="true">
-                <img class="swiper-slide" src="/public/assets/main-banner-1.png" alt="이 주의 특가 한 눈에 보기" />
+                <<img class="swiper-slide" src="/public/assets/main-banner-1.png" alt="이 주의 특가 한 눈에 보기" />>
                 <img class="swiper-slide" src="/public/assets/main-banner-2.png" alt="특가부터 인기 브랜드까지 최대 77% 할인 + 쿠폰팩" />
                 <div class="swiper-button-next"></div>
                 <div class="swiper-button-prev"></div>
