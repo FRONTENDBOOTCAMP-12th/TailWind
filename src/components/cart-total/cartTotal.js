0b2a6ac7cd49450c2c9bb0b4f2b2146423293beb
Littlestar0508 <rist9860@gmail.com>
2024-12-25 02:47:39 +0900

    Feature: 체크박스 변경 시 즉각적인 렌더링 유도
    - 완벽한 컴포넌트 분리 실패
    - 체크 표시했을때 다른 자식 컴포넌트에 대한 상태 변경 시 리렌더링이 일어나지 않음

--- src/components/cart-total/cartTotal.js
+++ src/components/cart-total/cartTotal.js
@@ -3,30 +3,13 @@ import '@/components/checkbox/checkbox.js';
 import cartTotalStyle from '@/components/cart-total/cartTotalStyle.js';
 import resetCss from '@/styles/reset.js';
 import itemCounter from '@/components/cart/cart.js';
+import { obj1 } from '@/components/cart/cart.js';
 
 class CartTotal extends LitElement {
     static styles = [resetCss, cartTotalStyle];
 
-    static properties = {
-        checkAll: { type: Boolean },
-    };
-
-    constructor() {
-        super();
-        this.checkAll = true;
-    }
-
     handleAllProuct() {
-        for (const value of Object.keys(itemCounter)) {
-            if (!itemCounter[value]) {
-                this.checkAll = false;
-                break;
-            }
-        }
-
-        console.log(this.checkAll);
-
-        if (!this.checkAll) {
+        if (!obj1['state']) {
             Object.keys(itemCounter).map((idx) => {
                 itemCounter[idx] = true;
             });
@@ -36,20 +19,24 @@ class CartTotal extends LitElement {
             });
         }
 
-        this.checkAll = !this.checkAll;
+        obj1['state'] = !obj1['state'];
         this.requestUpdate();
-    }
+        console.log(itemCounter);
 
-    updateList() {
-        this.requestUpdate();
+        this.dispatchEvent(
+            new CustomEvent('request-update', {
+                bubbles: true,
+                composed: true,
+            })
+        );
     }
 
     render() {
         return html`
             <div class="product-check-container">
-                <c-checkbox ?checked=${this.checkAll} @checkbox-change=${this.handleAllProuct}>
-                    <span @click=${this.handleChilck}>전체선택</span>
-                    <span @click=${this.updateList}
+                <c-checkbox ?checked=${obj1['state']} @checkbox-change=${this.handleAllProuct}>
+                    <span>전체선택</span>
+                    <span
                         >(${Object.entries(itemCounter).reduce((acc, cur) => {
                             return (acc += +cur[1]);
                         }, 0)}/${Object.keys(itemCounter).length})
