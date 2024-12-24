b8f0fafa10b9f1cb077835ad5442510145ae6568
Littlestar0508 <rist9860@gmail.com>
2024-12-25 02:51:11 +0900

    Commnet: 코드에 대한 주석 추가

--- src/components/cart-total/cartTotal.js
+++ src/components/cart-total/cartTotal.js
@@ -5,9 +5,13 @@ import resetCss from '@/styles/reset.js';
 import itemCounter from '@/components/cart/cart.js';
 import { obj1 } from '@/components/cart/cart.js';
 
+// obj1 은 전체 선택에 대한 state를 저장하는 객체 이후 이름 재조정 필요(컴포넌트 분리 시)
+// itemCounter는 각각의 product의 false와 true를 담당하는 객체
+
 class CartTotal extends LitElement {
     static styles = [resetCss, cartTotalStyle];
 
+    // 전체 체크 버튼을 클릭 시 만약 모두 true라면 false를 그렇지 않다면 모두 true로 변경(모든 id 값)
     handleAllProuct() {
         if (!obj1['state']) {
             Object.keys(itemCounter).map((idx) => {
@@ -19,10 +23,11 @@ class CartTotal extends LitElement {
             });
         }
 
+        // 전체 체크 버튼은 클릭 시 항상 본인의 상태와 반대로 설정
         obj1['state'] = !obj1['state'];
         this.requestUpdate();
-        console.log(itemCounter);
 
+        // 이후 상태가 변경됐다면 리렌더링 유도
         this.dispatchEvent(
             new CustomEvent('request-update', {
                 bubbles: true,
@@ -34,10 +39,12 @@ class CartTotal extends LitElement {
     render() {
         return html`
             <div class="product-check-container">
+                <!-- 체크 상태는 obj1의 state로 받아오기 그리고 체크박스가 변화됐다면 이벤트 발생 -->
                 <c-checkbox ?checked=${obj1['state']} @checkbox-change=${this.handleAllProuct}>
                     <span>전체선택</span>
-                    <span
-                        >(${Object.entries(itemCounter).reduce((acc, cur) => {
+                    <span>
+                        <!-- itemCounter의 key의 갯수와 true값을 계산 -->
+                        (${Object.entries(itemCounter).reduce((acc, cur) => {
                             return (acc += +cur[1]);
                         }, 0)}/${Object.keys(itemCounter).length})
                         |
