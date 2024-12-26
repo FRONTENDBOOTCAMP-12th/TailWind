20c7869e87a55320b2df3d0da103b2a3dc99c34b
Littlestar0508 <rist9860@gmail.com>
2024-12-26 16:24:17 +0900

    Feature: 증감 컴포넌트 사용 및 모달의 열림 유무 부모컴포넌트 조절 유도
    - 기본값으로 사용하던 변수제거
    - 취소 버튼 클릭시 수량 초기화
    - 확인 버튼 클릭시 localStorage에 저장

--- src/components/add-cart/addCart.js
+++ src/components/add-cart/addCart.js
@@ -1,6 +1,7 @@
 import { LitElement, html } from 'lit';
 import addCartStyle from '@/components/add-cart/addCartStyle.js';
 import reset from '@/styles/reset.js';
+import '@/components/inc-dec-component/incDecComponent.js';
 
 class AddCart extends LitElement {
     // resetCss가 정상적으로 적용된다면 js파일 분리 안할 예정(이건 상황에 따라 유동적으로 결정)
@@ -12,46 +13,43 @@ class AddCart extends LitElement {
         itemQuantity: { type: Number },
         productPrice: { type: Number },
         itemDesc: { type: String },
+        idx: { type: Object, attribute: 'idx' },
     };
 
     // 변수값 초기 설정
     constructor() {
         super();
+        this.itemQuantity = 1;
         this.isModalOpen = false;
-        this.itemQuantity = 0;
-        this.productPrice = 4980;
-        this.itemDesc = '[풀무원] 탱탱쫄면 (4개입)';
-    }
-
-    // TODO : 추후 컴포넌트 융합할 시 삭제 예정 이벤트
-    handleTest() {
-        this.isModalOpen = true;
     }
 
     // 취소 시 모달 창 닫는 이벤트
     handleCancel() {
-        this.isModalOpen = false;
-        this.itemQuantity = 0;
+        this.renderRoot.querySelector('inc-dec-btn').itemQuantity = 1;
+        this.itemQuantity = 1;
+
+        this.dispatchEvent(new CustomEvent('modal-close', { bubbles: true, composed: true }));
     }
 
     // 확인 시 모달 창 닫는 이벤트(TODO : 장바구니에 설정한 값을 저장)
     handleConfirm() {
-        this.isModalOpen = false;
-        console.log(this.itemQuantity);
-        this.itemQuantity = 0;
-        // TODO : 이후 상품을 추가하는 식을 사용
+        const cartItems = JSON.parse(localStorage.getItem('cartItems')) ?? {};
+        let itemValue = cartItems[this.idx['id']] ?? 0;
+        itemValue > 0
+            ? (itemValue += this.renderRoot.querySelector('inc-dec-btn').itemQuantity)
+            : (itemValue = this.renderRoot.querySelector('inc-dec-btn').itemQuantity);
+        cartItems[this.idx['id']] = itemValue;
+
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+        this.renderRoot.querySelector('inc-dec-btn').itemQuantity = 1;
+        this.itemQuantity = 1;
+
+        this.dispatchEvent(new CustomEvent('modal-close', { bubbles: true, composed: true }));
     }
 
-    // + 버튼 클릭 시 제품 수량 증가
-    // TODO : 컴포넌트 분리와 함께 addCart에선 제거 될 예정
-    handleIncreaseItem() {
-        this.itemQuantity++;
-    }
-
-    // - 버튼 클릭 시 제품 수량 감소
-    // TOOD : 컴포넌트 분리와 함께 addCart에선 제거 될 예정
-    handleDecreaseItem() {
-        this.itemQuantity--;
+    handleChange(e) {
+        this.itemQuantity = e.detail.itemQuantity;
     }
 
     render() {
@@ -60,38 +58,24 @@ class AddCart extends LitElement {
             <div class="add-cart  ${this.isModalOpen ? 'visible' : ''}">
                 <div class="add-cart-container">
                     <section>
-                        <h1 class="item-title">${this.itemDesc}</h1>
+                        <h2 class="item-title">${this.idx['name']}</h2>
                         <div class="total-price-container">
-                            <span class="item-price">${this.productPrice.toLocaleString()}원</span>
-                            <span class="amount-container">
-                                <!--만약 제품 수량이 1 미만이라면 - 버튼 비활성화-->
-                                <!-- TODO : 컴포넌트 분리와 함께 사라질 예정(증감 컴포넌트) -->
-                                <button
-                                    type="button"
-                                    class="minus-button"
-                                    @click=${this.handleDecreaseItem}
-                                    ?disabled=${this.itemQuantity < 1}
-                                    aria-label="제품 수량 감소 버튼"
-                                >
-                                    <svg width="10" height="2" viewBox="0 0 10 2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M10 0V2H0V0H10Z" fill="currentColor" />
-                                    </svg>
-                                </button>
-                                <span class="sr-only">제품 수량</span>
-                                <span class="item-amount">${this.itemQuantity}</span>
-                                <button type="button" class="plus-button" @click=${this.handleIncreaseItem} aria-label="제품 수량 증가 버튼">
-                                    <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M6 0V4H10V6H6V10H4V6H0V4H4V0H6Z" fill="currentColor" />
-                                    </svg>
-                                </button>
-                            </span>
+                            <span class="item-price"
+                                >${Math.floor(this.idx['price'] - this.idx['price'] * this.idx['discount'] * 0.01).toLocaleString()}원</span
+                            >
+                            <inc-dec-btn .itemQuantity=${this.itemQuantity} @quantity-change=${this.handleChange}></inc-dec-btn>
                         </div>
                     </section>
                     <section>
                         <div class="sum-container">
                             <h1 class="sum">합계</h1>
                             <!-- 제품 수량 * 제품 가격을 표시 -->
-                            <div class="total-price">${(this.itemQuantity * this.productPrice).toLocaleString()} 원</div>
+                            <div class="total-price">
+                                ${Math.floor(
+                                    (this.idx['price'] - this.idx['price'] * this.idx['discount'] * 0.01) * this.itemQuantity
+                                ).toLocaleString()}
+                                원
+                            </div>
                         </div>
                         <div class="saving-container">
                             <span class="saving">적립</span>
@@ -99,13 +83,12 @@ class AddCart extends LitElement {
                         </div>
                     </section>
                     <div class="add-confirm-container">
-                        <button type="button" class="add-cancel" @click=${this.handleCancel} aria-label="취소 버튼">취소</button>
-                        <button type="submit" class="add-confirm" @click=${this.handleConfirm} aria-label="제품 추가 버튼">장바구니 담기</button>
+                        <button type="button" class="add-cancel" @click=${this.handleCancel}>취소</button>
+                        <button type="submit" class="add-confirm" @click=${this.handleConfirm}>장바구니 담기</button>
                     </div>
                 </div>
             </div>
             <!-- 이후 삭제될 예정 -->
-            <button type="button" class="test" @click=${this.handleTest}>모달 테스트</button>
         `;
     }
 }
