2250fcc71bae8015521628b9da2b3d97b3d5e233
Littlestar0508 <rist9860@gmail.com>
2024-12-26 14:09:53 +0900

    Modify: 상품의 id만 받아서 일괄적으로 렌더링 되도록 수정

--- src/components/product-card/productCard.js
+++ src/components/product-card/productCard.js
@@ -1,29 +1,20 @@
 import { LitElement, html } from 'lit';
 import reset from '@/styles/reset.js';
 import productCardStyle from '@/components/product-card/productCardStyle.js';
+import { fileUrl } from '@/api/pockethost.js';
 
 class ProductCard extends LitElement {
     static properties = {
         // TODO : Get요청을 함에 따라 변하는 변수들 이후 attribute는 삭제해도 됨
-        src: { type: String },
-        specialDesc: { type: String },
-        productName: { type: String },
-        discount: { type: Number },
-        price: { type: Number },
-        desc: { type: String },
+        idx: { type: Object, attribute: 'idx' },
     };
 
-    constructor() {
-        super();
-        this.src = '/assets/product01.png';
-        this.specialDesc = '제품 특별 설명';
-        this.productName = '제품 명';
-        this.discount = 15;
-        this.price = 21900;
-        this.desc = '제품 간단 설명';
-    }
     // 스타일 지정
     static styles = [reset, productCardStyle];
+
+    handleClick() {
+        console.log(this.idx);
+    }
 
     // HTML 렌더 부분
     render() {
@@ -31,25 +22,30 @@ class ProductCard extends LitElement {
             <section class="product-card-container">
                 <div class="img-container">
                     <!-- TODO: 이미지 경로는 상황에 맞게 수정 -->
-                    <img src="${this.src}" alt="상품 이미지" class="product-img" />
+                    <img
+                        src="${fileUrl + this.idx['id'] + '/' + this.idx['main_image']}"
+                        alt="${this.idx['name']} 이미지"
+                        class="product-img"
+                        @click=${this.handleClick}
+                    />
                     <button type="button" aria-label="장바구니에 추가하기" class="save-item"></button>
                 </div>
                 <!-- TODO: 이동하는 링크를 이후에 아이템에 맞춰 설정하여 렌더링 -->
                 <a href="/" class="text-container">
                     <!--TODO : 여기에 사용되는 모든 텍스트와 가격,할인율은 이후 get요청으로 받아오기-->
                     <!-- 접근성을 고려햐여 할인된 가격과 원래 가격, 할인율이 각각 무엇에 해당하는지 명시 -->
-                    <span class="special-desc">${this.specialDesc}</span>
-                    <h1 class="title">${this.productName}</h1>
+                    <span class="special-desc">${this.idx['category']}</span>
+                    <h2 class="title">${this.idx['name']}</h2>
                     <div>
                         <span class="sr-only">할인율</span>
-                        <span class="discount">${this.discount}%</span>
+                        <span class="discount">${this.idx['discount']}%</span>
                         <!--TODO : 실제 판매 가격(할인된 가격)은 계산식으로 처리할 예정-->
                         <span class="sr-only">할인된 가격</span>
-                        <span class="real-price">${(this.price - this.price * this.discount * 0.01).toLocaleString()} 원</span>
+                        <span class="real-price">${(this.idx['price'] - this.idx['price'] * this.idx['discount'] * 0.01).toLocaleString()} 원</span>
                     </div>
                     <span class="sr-only">원래 가격</span>
-                    <span class="price">${this.price.toLocaleString()} 원</span>
-                    <span class="desc">${this.desc}</span>
+                    <span class="price">${this.idx['price'].toLocaleString()} 원</span>
+                    <span class="desc">${this.idx['description']}</span>
                     <!--TODO : 이를 데이터에 추가적으로 저장해서 넘기기-->
                     <div class="tag">
                         <span>Karly Only</span>
