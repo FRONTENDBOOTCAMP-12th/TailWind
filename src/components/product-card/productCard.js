b38d16f1a191aff0af252e751de594616de093d2
Littlestar0508 <rist9860@gmail.com>
2024-12-26 16:19:41 +0900

    Modify: 제품의 id를 받아와서 처리하도록 유도
    - 부모 컴포넌트의 사용 간편화
    - karly only 와 한정 수량에 카드에 대한 데이터 처리
    - 장바구니 추가 모달을 위한 사전 작업

--- src/components/product-card/productCard.js
+++ src/components/product-card/productCard.js
@@ -2,18 +2,35 @@ import { LitElement, html } from 'lit';
 import reset from '@/styles/reset.js';
 import productCardStyle from '@/components/product-card/productCardStyle.js';
 import { fileUrl } from '@/api/pockethost.js';
+import '@/components/add-cart/addCart.js';
 
 class ProductCard extends LitElement {
     static properties = {
         // TODO : Get요청을 함에 따라 변하는 변수들 이후 attribute는 삭제해도 됨
         idx: { type: Object, attribute: 'idx' },
+        hidden: { type: Boolean },
+        isOpen: { type: Boolean },
     };
+
+    constructor() {
+        super();
+        this.hidden = false;
+        this.isOpen = false;
+    }
 
     // 스타일 지정
     static styles = [reset, productCardStyle];
 
     handleClick() {
         console.log(this.idx);
+    }
+
+    handleOpenCartModal() {
+        this.isOpen = true;
+    }
+
+    closeModal() {
+        this.isOpen = false;
     }
 
     // HTML 렌더 부분
@@ -28,7 +45,7 @@ class ProductCard extends LitElement {
                         class="product-img"
                         @click=${this.handleClick}
                     />
-                    <button type="button" aria-label="장바구니에 추가하기" class="save-item"></button>
+                    <button type="button" aria-label="장바구니에 추가하기" class="save-item" @click=${this.handleOpenCartModal}></button>
                 </div>
                 <!-- TODO: 이동하는 링크를 이후에 아이템에 맞춰 설정하여 렌더링 -->
                 <a href="/src/pages/ProductDetail/index.html?id=${this.idx['id']}" class="text-container">
@@ -38,21 +55,26 @@ class ProductCard extends LitElement {
                     <h2 class="title">${this.idx['name']}</h2>
                     <div>
                         <span class="sr-only">할인율</span>
-                        <span class="discount">${this.idx['discount']}%</span>
+                        <span class="discount" ?hidden=${this.idx['discount'] > 0 ? this.hidden : !this.hidden}>${this.idx['discount']}%</span>
                         <!--TODO : 실제 판매 가격(할인된 가격)은 계산식으로 처리할 예정-->
                         <span class="sr-only">할인된 가격</span>
-                        <span class="real-price">${(this.idx['price'] - this.idx['price'] * this.idx['discount'] * 0.01).toLocaleString()} 원</span>
+                        <span class="real-price"
+                            >${Math.floor(this.idx['price'] - this.idx['price'] * this.idx['discount'] * 0.01).toLocaleString()} 원</span
+                        >
                     </div>
                     <span class="sr-only">원래 가격</span>
-                    <span class="price">${this.idx['price'].toLocaleString()} 원</span>
+                    <span class="price" ?hidden=${this.idx['discount'] > 0 ? this.hidden : !this.hidden}
+                        >${this.idx['price'].toLocaleString()} 원</span
+                    >
                     <span class="desc">${this.idx['description']}</span>
                     <!--TODO : 이를 데이터에 추가적으로 저장해서 넘기기-->
                     <div class="tag">
-                        <span>Karly Only</span>
-                        <span>한정 수량</span>
+                        <span ?hidden=${this.idx['karlyOnly']}>Karly Only</span>
+                        <span ?hidden=${this.idx['limitedQuantity']}>한정 수량</span>
                     </div>
                 </a>
             </section>
+            <add-cart ?isModalOpen=${this.isOpen} idx=${JSON.stringify(this.idx)} @modal-close=${this.closeModal}></add-cart>
         `;
     }
 }
