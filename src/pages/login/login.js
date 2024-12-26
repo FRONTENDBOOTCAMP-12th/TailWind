510c69d3a0409126dae87e42cc8f137a3969f9c2
betteree <kkne1017@gmail.com>
2024-12-26 10:12:41 +0900

    Modify: 로그인 페이지 접근성 이슈
    - id 레이블 추가
    - id, pw 유효성 갖추어야하지만 서버 전송 가능 (통신방지)
    - button type 수정
    
    resolves #137

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,12 +3,16 @@ import resetCss from '@/styles/reset.js';
 import loginCss from './loginCss';
 import { pb } from '@/api/pockethost.js';
 class Login extends LitElement {
+    static properties = {
+        hint: { type: Boolean },
+    };
     constructor() {
         super();
         this.inputs = {
             idField: '',
             pwField: '',
         };
+        this.hint = false;
     }
 
     static get styles() {
@@ -23,17 +27,40 @@ class Login extends LitElement {
         const value = input.value;
 
         this.inputs[id] = value;
+
+        this.handleHint(e);
+    }
+    // 유효성을 갖추었는지 값 가져오기
+    handleHint(e) {
+        const errorMessage = getComputedStyle(
+            e
+                .composedPath()
+                .find((el) => el.classList?.contains('input-container'))
+                ?.querySelector('.error-message')
+        ).display;
+
+        //display가 none이면 제대로 입력했다는 뜻
+        if (errorMessage === 'none') {
+            this.hint = true;
+        } else {
+            this.hint = false;
+        }
     }
 
+    //로그인 시도 함수
     async handleLogin() {
-        try {
-            const id = this.inputs['idField'];
-            const pw = this.inputs['pwField'];
+        if (this.hint) {
+            try {
+                const id = this.inputs['idField'];
+                const pw = this.inputs['pwField'];
 
-            await pb.collection('users').authWithPassword(id, pw);
-            alert('성공');
-        } catch {
-            alert('틀렸어');
+                await pb.collection('users').authWithPassword(id, pw);
+                alert('성공');
+            } catch {
+                alert('실패');
+            }
+        } else {
+            alert('제대로 입력하세요');
         }
     }
 
@@ -42,17 +69,22 @@ class Login extends LitElement {
             <h2 class="login-title">로그인</h2>
 
             <div class="login-form">
+
+                <label for="idField" class="sr-only">아이디</label>
                 <c-input 
                 placeholder="아이디"
                 id="idField"
                 @input="${this.handleInput}" 
-                            .validation=${null} 
+                            errorMessage="숫자만 입력 불가능, 6자 이상"
+                            .validation=${/^(?=.*\D).{6,}$/}
                             required></c-input>
+                <label for="pwField" class="sr-only">비밀번호</label>
                 <c-input 
                 placeholder="비밀번호"  
                 id="pwField"
                 @input="${this.handleInput}" 
-                         .validation=${null}    
+                errorMessage="특수문자 포함 최소 6자 이상 16자 이하의 영문"
+                .validation=${/^(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z0-9!@#$%^&*(),.?":{}|<>]{6,16}$/}  
                          required></c-input>
 
                 <span class="find-st">
@@ -61,8 +93,8 @@ class Login extends LitElement {
                     <a href="/">비밀번호 찾기</a>
                 </span>
 
-                <c-button type="button" mode="fill" size="btn-md" @click ="${this.handleLogin}">로그인</c-button>
-                <c-button type="button" mode="outline" size="btn-md">회원가입</c-button></c-button>
+                <c-button type="submit" mode="fill" size="btn-md" @click ="${this.handleLogin}">로그인</c-button>
+                <c-button type="link" mode="outline" size="btn-md">회원가입</c-button></c-button>
             </div>
         </div>`;
     }
