a54254b805d5f209ebfbeac5c812ac269619f6bd
jimyeong21 <jimyeong21@gmail.com>
2024-12-27 15:59:35 +0900

    Modify: px to rem
    - px to rem
    
    resolves #151

--- src/components/ProductPagination/ProductPaginationStyle.js
+++ src/components/ProductPagination/ProductPaginationStyle.js
@@ -3,23 +3,22 @@ import { css } from 'lit';
 export default css`
     .product-pagination {
         display: flex;
-        margin: 60px auto 0;
+        margin: 3.75rem auto 0;
         width: fit-content;
         border: 1px solid #e1e1e1;
 
         & li {
             & button {
                 text-align: center;
-                line-height: 36px;
-                width: 34px;
-                height: 34px;
+                line-height: 2.25rem;
+                width: 2.125rem;
+                height: 2.125rem;
                 color: var(--content);
-                font-size: 12px;
+                font-size: 0.75rem;
                 cursor: pointer;
             }
         }
-
-        & li + li {
+        å & li + li {
             border-left: 1px solid #e1e1e1;
         }
     }
