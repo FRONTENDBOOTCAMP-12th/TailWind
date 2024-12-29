1887eb4929ac8fe153f25b8ff6d05af39012651c
lyb9030 <lyb_9030@naver.com>
2024-12-30 02:40:37 +0900

    Modify : 메인 배너 스와이퍼 구현

--- src/components/swiper/swiper.js
+++ src/components/swiper/swiper.js
@@ -5,94 +5,20 @@ import { SwiperStyles } from './swiperStyle';
 
 register();
 
-class ProductSwiper extends LitElement {
+class BannerSwiper extends LitElement {
     static styles = [reset, SwiperStyles];
 
     render() {
         return html`
-            <body>
-                <swiper-container autoplay="true" autoplay-delay="3000" disableOnInteraction="true" navigation="true" pagination="true" loop="true">
-                    <img class="swiper-slide" src="/public/assets/main-banner-1.png" alt="이 주의 특가 한 눈에 보기" />
-                    <img class="swiper-slide" src="/public/assets/main-banner-2.png" alt="특가부터 인기 브랜드까지 최대 77% 할인 + 쿠폰팩" />
-                    <div class="swiper-button-next"></div>
-                    <div class="swiper-button-prev"></div>
-                </swiper-container>
-                <hr />
-                <h3>이 상품 어때요?</h3>
-                           <!-- 상품정보는 componets의 product-card를 get 예정 -->
-                           <swiper-container disableOnInteraction="true" navigation="true">
-                           
-                           <ul class="itembox">
-                           <li>
-                               <img src="/public/item1.png" />
-                               <div class="iteminfo">
-                                   <p class="itembrand">[풀무원]</p>
-                                   <p class="itemname">탱탱쫄면 (4개입)</p>
-                                   <div class="discount">
-                                       <p class="discountrate">20%</p>
-                                       <p class="discountprice">3,000원</p>
-                                   </div>
-                                   <p class="itemprice">5,000원</p>
-                                   <p class="itemexplanation">풀무원의 탱탱하고 맛있는 쫄면</p>
-                               </div>
-                           </li>
-                           <li>
-                               <img src="/public/item2.png" />
-                               <div class="iteminfo">
-                                   <p class="itembrand">[풀무원]</p>
-                                   <p class="itemname">탱탱쫄면 (4개입)</p>
-                                   <div class="discount">
-                                       <p class="discountrate">20%</p>
-                                       <p class="discountprice">3,000원</p>
-                                   </div>
-                                   <p class="itemprice">5,000원</p>
-                                   <p class="itemexplanation">풀무원의 탱탱하고 맛있는 쫄면</p>
-                               </div>
-                           </li>
-               
-                           <li>
-                               <img src="/public/item1.png" />
-                               <div class="iteminfo">
-                                   <p class="itembrand">[풀무원]</p>
-                                   <p class="itemname">탱탱쫄면 (4개입)</p>
-                                   <div class="discount">
-                                       <p class="discountrate">20%</p>
-                                       <p class="discountprice">3,000원</p>
-                                   </div>
-                                   <p class="itemprice">5,000원</p>
-                                   <p class="itemexplanation">풀무원의 탱탱하고 맛있는 쫄면</p>
-                               </div>
-                           </li>
-                           <li>
-                               <img src="/public/item2.png" />
-                               <div class="iteminfo">
-                                   <p class="itembrand">[풀무원]</p>
-                                   <p class="itemname">탱탱쫄면 (4개입)</p>
-                                   <div class="discount">
-                                       <p class="discountrate">20%</p>
-                                       <p class="discountprice">3,000원</p>
-                                   </div>
-                                   <p class="itemprice">5,000원</p>
-                                   <p class="itemexplanation">풀무원의 탱탱하고 맛있는 쫄면</p>
-                               </div>
-                           </li>
-                           
-                       </ul>
-                       <div class="swiper-button-next"></div>
-                       <div class="swiper-button-prev"></div>
-                           </swiper-container>
-    </div>
-    <hr />
-    <!-- 배너 -->
-    <!-- 하이퍼 링크 나중에 추가하기 -->
-    <div class="banner2">
-        <img src="/public/assets/medium-banner.png" alt="10월 컬리마켓 할인 적립률 up + 3종 쿠폰팩" />
-    </div>
+            <swiper-container autoplay="true" autoplay-delay="3000" disableOnInteraction="true" navigation="true" pagination="true" loop="true">
+                <img class="swiper-slide" src="/public/assets/main-banner-1.png" alt="이 주의 특가 한 눈에 보기" />
 
-    <hr />
-            </body>
+                <img class="swiper-slide" src="/public/assets/main-banner-2.png" alt="특가부터 인기 브랜드까지 최대 77% 할인 + 쿠폰팩" />
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+            </swiper-container>
         `;
     }
 }
 
-customElements.define('product-swiper', ProductSwiper);
+customElements.define('banner-swiper', BannerSwiper);
--- src/components/swiper/swiperStyle.js
+++ src/components/swiper/swiperStyle.js
@@ -1,7 +1,7 @@
 import { css } from 'lit';
 
 export const SwiperStyles = css`
-    /* 사이드바 컴포넌트 스타일 */
+    /* 스와이퍼 컴포넌트 스타일 */
     .mainbanner {
         margin: 0 auto;
         display: flex;
@@ -15,81 +15,11 @@ export const SwiperStyles = css`
         }
     }
 
-    h3 {
-        text-align: center;
-    }
-
-    .itembox {
-        list-style: none;
-        display: flex;
-        justify-content: center;
-
-        & img {
-            width: 250px;
-            height: 320px;
-        }
-
-        .iteminfo {
-            padding: 10px;
-            margin: 10 auto;
-            display: block;
-            text-align: left;
-            width: 250px;
-            justify-content: center;
-            font-size: 14px;
-
-            .itembrand {
-                font-weight: 700;
-                color: gray;
-                margin: 0;
-            }
-
-            .itemname {
-                font-weight: 700;
-                color: black;
-                margin: 5px 0;
-            }
-
-            .discount {
-                display: flex;
-                margin: 0;
-
-                .discountrate {
-                    font-weight: 700;
-                    color: red;
-                    padding-right: 10px;
-                }
-
-                .discountprice {
-                    font-weight: 700;
-                }
-            }
-
-            .itemprice {
-                color: gray;
-                text-decoration: line-through;
-                margin: 0;
-            }
-        }
-    }
-
-    .banner2 {
-        margin: 0 auto;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 65.625rem;
-        height: 13.6875rem;
-
-        & img {
-            max-width: 100%;
-        }
-    }
-
     .swiper {
         width: 100%;
         height: 300px;
     }
+
     .swiper-slide {
         display: flex;
         align-items: center;
