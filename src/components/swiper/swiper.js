4befc11d6272c6cdd1a1db7e4d6eba97f6e5c6c0
lyb9030 <lyb_9030@naver.com>
2024-12-30 18:32:55 +0900

    BugFix : 배너 랜더링 오류 수정

--- src/components/swiper/swiper.js
+++ src/components/swiper/swiper.js
@@ -11,11 +11,9 @@ class BannerSwiper extends LitElement {
     render() {
         return html`
             <swiper-container autoplay="true" autoplay-delay="3000" disableOnInteraction="true" navigation="true" pagination="true" loop="true">
-                <img class="swiper-slide" src="/public/assets/main-banner-1.png" alt="이 주의 특가 한 눈에 보기" />
+                <img class="swiper-slide" src="/assets/main-banner-1.png" alt="이 주의 특가 한 눈에 보기" />
 
-                <img class="swiper-slide" src="/public/assets/main-banner-2.png" alt="특가부터 인기 브랜드까지 최대 77% 할인 + 쿠폰팩" />
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
+                <img class="swiper-slide" src="/assets/main-banner-2.png" alt="특가부터 인기 브랜드까지 최대 77% 할인 + 쿠폰팩" />
             </swiper-container>
         `;
     }
