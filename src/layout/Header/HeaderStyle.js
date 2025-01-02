464b966cca75cfdd0fc133e74610e68bbc72b6d2
Littlestar0508 <rist9860@gmail.com>
2025-01-02 11:04:47 +0900

    Design: 스티키 헤더에도 변경내용 적용

--- src/layout/Header/HeaderStyle.js
+++ src/layout/Header/HeaderStyle.js
@@ -138,16 +138,17 @@ export default css`
                 position: absolute;
                 top: 0;
                 right: 0;
-                display: block;
-                width: 0.8125rem;
-                height: 0.8125rem;
+                display: flex;
+                width: 1rem;
+                height: 1rem;
                 border-radius: 50%;
                 background: var(--accent--yellow);
                 color: var(--white);
                 font-size: 0.75rem;
                 line-height: 1.4;
                 font-weight: 600;
-                text-align: center;
+                align-items: center;
+                justify-content: center;
             }
         }
 
